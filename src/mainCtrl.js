272ddd1b499ae4061a91d4729085415fe46f4f39
Hirthanan Subenderan <Hirthanan_97@hotmail.com>
2016-06-06 21:52:26 -0400

    responsive navBar + ctrl added
    finished navigation bar

--- src/mainCtrl.js
+++ src/mainCtrl.js
@@ -2,7 +2,10 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('navCtrl', ['$scope', function ($scope) {
     //use this notation to declare function inside the javascript file
-    $scope.greeting = "Hola!";
+
+    $scope.myFunction = function myFunction() {
+        document.getElementsByClassName("navBar")[0].classList.toggle("responsive");
+    }
 
 
 }
