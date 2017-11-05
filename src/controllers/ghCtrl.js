63367469f574c5aae2c7e7c3d38a3f69af7e210f
Hirthanan Subenderan <Hirthanan_97@hotmail.com>
2017-11-05 02:33:02 -0500

    dynamically retrieve my gh repos using ghapi

---
+++ src/controllers/ghCtrl.js
@@ -0,0 +1,25 @@
+
+angular.module('myApp.controllers').controller('ghCtrl', function($scope, $http) {
+
+    $scope.URL = "https://api.github.com/users/hirthanan/";
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    // GET list of hirthanan's repos
+    $http.get($scope.URL+"repos").
+        then(function(repoData) {
+            projects = [];
+            repoData = repoData.data;
+            for (var i = repoData.length - 1; i >= 0; i--) {
+                // skip these repos
+                if (repoData[i].name == "Switch-Statement-Assignment") {
+                    continue;
+                }
+                projects.push(capitalizeFirstLetter(repoData[i].name));
+            }
+            $scope.repoData = repoData;
+            $scope.projects = projects;
+        });
+});
--- src/controllers/mainCtrl.js
+++ src/controllers/mainCtrl.js
@@ -1,6 +1,5 @@
-var myApp = angular.module('myApp', []);
 
-myApp.controller('navCtrl', ['$scope', function ($scope) {
+angular.module('myApp.controllers').controller('navCtrl', ['$scope', function ($scope) {
     //use this notation to declare function inside the javascript file
 
     $scope.myFunction = function myFunction() {
